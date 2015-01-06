2dc2bfd24b7f3e1f582486f8b54bb64f1dbd0ed5
kmooney <kmooney@gmail.com>
2015-01-06 17:31:35 -0600

    Changes for Django 1.7

--- ui_patterns/urls.py
+++ ui_patterns/urls.py
@@ -1,9 +1,17 @@
-from django.conf.urls.defaults import *
-from .views import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
+from .views import PatternCategoryView, PatternDetailView, IndexView
 
-  url(r'^group/(?P<category>.*)/$', PatternCategoryView.as_view(), name="category"),
-  url(r'^(?P<slug>.*)/$', PatternDetailView.as_view(), name="detail"),
-  url(r'^$', IndexView.as_view(), name="index"),
+urlpatterns = patterns(
+    '',
+    url(
+        r'^group/(?P<category>.*)/$',
+        PatternCategoryView.as_view(),
+        name="category"
+    ),
+    url(r'^(?P<slug>.*)/$', PatternDetailView.as_view(), name="detail"),
+    url(r'^$', IndexView.as_view(), name="index"),
 )
